4969456f55c92e909c37897d3a15145c8190e531
Fernando2609 <jfortiz@unah.hn>
2022-01-02 21:00:44 -0600

    Se revirtieron los cambios,Login Arreglado y finalizado

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -3,15 +3,13 @@
         public function __construct()
         {
             parent::__construct();
-
             session_start();
-            //session_regenerate_id(true);
+            session_regenerate_id(true);
             if (empty($_SESSION['login'])) {
                 header('Location: '.base_url().'/login');
                 die();
             }
             getPermisos(1);
-
             
         }
         
