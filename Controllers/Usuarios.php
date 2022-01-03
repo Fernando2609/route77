33191f25f5827a2b6d12d27b7257b7db0bf1bbdc
José Fernando Ortiz Santos <74040616+Fernando2609@users.noreply.github.com>
2022-01-02 21:59:01 -0600

    Revert "Se resolvieron los problemas, version final del calendario arreglado"

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -3,12 +3,17 @@
         public function __construct()
         {
             parent::__construct();
+
             session_start();
+
+            //session_regenerate_id(true);
+
             if (empty($_SESSION['login'])) {
                 header('Location: '.base_url().'/login');
                 die();
             }
             getPermisos(2);
+
         }
         
         public function Usuarios()
--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -60,7 +60,8 @@
 <script src="<?= media(); ?>/js/<?= $data['page_functions_js'] ?>"></script>
 
 
-<!-- <script type="text/javascript" src="<?= media();?>/js/funtions_admin.js"></script> -->
+ <script type="text/javascript" src="<?= media();?>/js/funtions_admin.js"></script>  
+
 
 <script>
   const imgB64="<?=  img64();  ?>"
