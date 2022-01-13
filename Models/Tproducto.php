36e8dcafdbc57325ac33f66af4dd0d2dda374f22
GabyMaradiaga <ggmaradiaga@unah.hn>
2022-01-13 01:08:05 -0600

    Tienda Productos Terminado

--- Models/Tproducto.php
+++ Models/Tproducto.php
@@ -4,6 +4,9 @@ trait Tproducto{
     private $con;
     private $strCategoria;
     private $intIdCategoria;
+    private $strProducto;
+    private $cant;
+    private $option;
    
     public function getProductosT(){
         $this->con = new Mysql();
@@ -18,7 +21,7 @@ trait Tproducto{
                 FROM producto p 
                 INNER JOIN categoria c
                 ON p.categoriaid = c.idcategoria
-                WHERE p.status != 0 ";
+                WHERE p.status != 0 ORDER BY p.idproducto DESC ";
                 $request = $this->con->select_all($sql);
                 if(count($request) > 0){
                     for ($c=0; $c < count($request) ; $c++) { 
@@ -79,5 +82,90 @@ trait Tproducto{
      
         return $request;
     }
+    public function getProductoT(string $producto)
+    {
+        $this->con = new Mysql();
+        $this->strProducto = $producto;
+        $sql = "SELECT p.idproducto,
+                        p.codigo,
+                        p.nombre,
+                        p.descripcion,
+                        p.categoriaid,
+                        c.nombre as categoria,
+                        p.precio,
+                        p.stock 
+                FROM producto p 
+                INNER JOIN categoria c
+                ON p.categoriaid = c.idcategoria
+                WHERE p.status != 0 AND p.nombre = '{$this->strProducto}' ";
+        $request = $this->con->select($sql);
+         if (!empty($request)) {
+                 $intIdProducto = $request['idproducto'];
+                 $sqlImg = "SELECT img
+                                 FROM imagen
+                                 WHERE productoid = $intIdProducto";
+                 $arrImg = $this->con->select_all($sqlImg);
+                 if (count($arrImg) > 0) {
+                     for ($i = 0; $i < count($arrImg); $i++) {
+                         $arrImg[$i]['url_image'] = media() . '/images/uploads/' . $arrImg[$i]['img'];
+                     }
+                 }
+                 $request['images'] = $arrImg;
+         }
+        return $request;
+    }
+    public function getProductosRandom(int $idcategoria, int $cant, string $option){
+        $this->intIdCategoria =$idcategoria;
+        $this->cant = $cant;
+        $this->option = $option;
+
+        if ($option == "r") {
+            $this->option = " RAND() ";
+        }else if
+        ($option == "a"){
+            $this->option = "idproducto ASC ";
+        }else{
+            $this->option = "idproducto DESC ";
+        }
+
+        $this->con = new Mysql();
+            $sql = "SELECT p.idproducto,
+            p.codigo,
+            p.nombre,
+            p.descripcion,
+            p.categoriaid,
+            c.nombre as categoria,
+            p.precio,
+            p.stock 
+            FROM producto p 
+            INNER JOIN categoria c
+            ON p.categoriaid = c.idcategoria
+            WHERE p.status != 0  AND p.categoriaid = $this->intIdCategoria
+            ORDER BY $this->option LIMIT $this->cant ";
+            $request = $this->con->select_all($sql);
+            if (count($request) > 0) {
+                for ($c = 0; $c < count($request); $c++) {
+                    $intIdProducto = $request[$c]['idproducto'];
+                    $sqlImg = "SELECT img
+                            FROM imagen
+                            WHERE productoid = $intIdProducto";
+                    $arrImg = $this->con->select_all($sqlImg);
+                    if (count($arrImg) > 0) {
+                        for ($i = 0; $i < count($arrImg); $i++) {
+                            $arrImg[$i]['url_image'] = media() . '/images/uploads/' . $arrImg[$i]['img'];
+                        }
+                    }
+                    $request[$c]['images'] = $arrImg;
+                }
+            }
+        
+
+        return $request;
+
+
+
+    }
+
+
 }
 ?>
